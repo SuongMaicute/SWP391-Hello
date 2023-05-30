a7f45544e9b74e23b8083c11fd2a537cf5dc8d0e
whitedolpin <quanhmse170196@fpt.edu.vn>
2023-05-30 14:47:29 +0700

    merged code May 30 2:47 PM

---
+++ BirdPlatformHello/src/java/com/birdtradingplatform/controller/OrderController.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.birdtradingplatform.controller;
+
+import com.birdtradingplatform.dao.OrderDAO;
+import com.birdtradingplatform.model.Account;
+import com.birdtradingplatform.model.OrderHistory;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author leyen
+ */
+@WebServlet(name = "OrderController", urlPatterns = {"/order"})
+public class OrderController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        String action = request.getParameter("action");
+        if ("historyorder".equals(action)) {
+            HttpSession session = request.getSession();
+            String status = request.getParameter("status");
+            //get account login in this session with attribute name is LOGIN_ACCOUNT
+            Account account = (Account) session.getAttribute("LOGIN_ACCOUNT");
+            
+            OrderDAO odao = new OrderDAO();
+            List<OrderHistory> orderList = odao.getOrderHistory(account.getAccountID(), status);
+            double totalMoney = 0;
+            int totalOrder = orderList.size();            
+            for (OrderHistory order : orderList) {
+                totalMoney += order.getTotal();
+            }
+            
+            request.setAttribute("ORDER_LIST", orderList);
+            request.setAttribute("TOTAL_MONEY", totalMoney);
+            request.setAttribute("TOTAL_ORDER", totalOrder);
+            request.getRequestDispatcher("orderhistory.jsp").forward(request, response);
+                   
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ BirdPlatformHello/src/java/com/birdtradingplatform/dao/RoleDAO.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.birdtradingplatform.dao;
+
+import com.birdtradingplatform.model.Account;
+import com.birdtradingplatform.model.Role;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.birdtradingplatform.dao.AccountDAO;
+
+import minhquan.util.DBHelper;
+
+/**
+ *
+ * @author Minh Quan
+ */
+public class RoleDAO {
+    
+    private ArrayList<Role> roles;
+
+    /**
+     * @return the roles
+     */
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
+    public ArrayList<Role> getAllRoles(List<Account> userList) throws ClassNotFoundException, SQLException{
+         Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            con = DBHelper.makeConnection();
+            if (con != null) {
+                String sql = "SELECT * FROM ROLE WHERE role = ?";
+                stm = con.prepareStatement(sql);
+                for (Account user : userList) {
+                    stm.setInt(1, user.getRole());
+                    rs = stm.executeQuery();
+                
+                while (rs.next()) {
+                    int role = rs.getInt("role");
+                    String roleName = rs.getString("roleName");
+                    Role result = new Role(role, roleName);
+                    if (this.roles == null) {
+                        this.roles = new ArrayList<>();
+                    }
+                    this.roles.add(result);
+                }
+            }
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+
+        }
+        return this.roles;        
+    }
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        AccountDAO dao = new AccountDAO();
+        List<Account> user = dao.getUserList();
+        RoleDAO roleDAO = new RoleDAO();
+        roleDAO.getAllRoles(user);
+        List<Role> roles = roleDAO.getRoles();
+        System.out.println(roles);
+    }
+}
